da83c0f3b08e97d22f2447d34541c4255674860e
Rita Cascante <margalida.kaskante@gmail.com>
2018-01-04 21:04:09 +0100

    Calculator

--- js/main.js
+++ js/main.js
@@ -34,11 +34,48 @@ init();
 //CALCULATOR
 var hoursInput = document.querySelector('#hours-input');
 var buttonCalc = document.querySelector('#submit-input');
+var hourValueData = document.querySelector('#hour-value');
+var monthValueData = document.querySelector('#salary-value');
 
 buttonCalc.addEventListener('click',calculator);
 
 function calculator() {
   var hours = hoursInput.value;
   var numberHours = parseInt(hours);
-  if ()
+  if (numberHours > 40) {
+    alert('El máximo de horas laborales legalmente permitido es de 40 horas semanales');
+  }
+  else if (numberHours <= 40 && numberHours > 35 ) {
+    hourValueData.innerHTML = '5.50€';
+    monthValueData.innerHTML = '880.72€';
+    hoursInput.value = '';
+  } else if (numberHours <= 35 && numberHours > 30) {
+    hourValueData.innerHTML = '5.46€';
+    monthValueData.innerHTML = '764.12€';
+    hoursInput.value = '';
+  } else if (numberHours <= 30 && numberHours > 25) {
+    hourValueData.innerHTML = '5.44€';
+    monthValueData.innerHTML = '653.39€';
+    hoursInput.value = '';
+  } else if (numberHours >= 24 && numberHours <= 25) {
+    hourValueData.innerHTML = '5.42€';
+    monthValueData.innerHTML = '542.66€';
+    hoursInput.value = '';
+  } else if (numberHours <= 20 && numberHours > 18 ) {
+    hourValueData.innerHTML = '5.40€';
+    monthValueData.innerHTML = '431.93€';
+    hoursInput.value = '';
+  } else if (numberHours <= 18 && numberHours > 16) {
+    hourValueData.innerHTML = '5.512€';
+    monthValueData.innerHTML = '396.93€';
+    hoursInput.value = '';
+  } else if (numberHours <= 16 && numberHours > 14) {
+    hourValueData.innerHTML = '5.65€';
+    monthValueData.innerHTML = '361.94€';
+    hoursInput.value = '';
+  } else {
+    hourValueData.innerHTML = '5.37€';
+    monthValueData.innerHTML = '260.71€';
+    hoursInput.value = '';
+  }
 }
